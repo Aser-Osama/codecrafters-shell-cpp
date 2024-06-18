45fcb4f37cce85dac03a999a6885e3f07d125e23
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-06-18 23:51:30 +0300

    task repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-int main()
-{
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while (1) {
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+    std::string input;
+    std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << std::endl;
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
